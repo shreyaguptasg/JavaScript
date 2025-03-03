41bfa58fa849a6c60d6aa5946c0c3231dd00c15e
shrey <shreya272002gupta@gmail.com>
2025-03-03 14:01:02 +0530

    [Shreya] Add. display numbers in words

---
+++ JSConstructs/SeclectionProblemWithCaseStatements/NumberInWord.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a single-digit number (0-9): ", (num) => {
+    num = parseInt(num);
+
+    switch (num) {
+        case 0: console.log("Zero"); break;
+        case 1: console.log("One"); break;
+        case 2: console.log("Two"); break;
+        case 3: console.log("Three"); break;
+        case 4: console.log("Four"); break;
+        case 5: console.log("Five"); break;
+        case 6: console.log("Six"); break;
+        case 7: console.log("Seven"); break;
+        case 8: console.log("Eight"); break;
+        case 9: console.log("Nine"); break;
+        default: console.log("Invalid input! Please enter a single digit (0-9).");
+    }
+
+    rl.close();
+});
